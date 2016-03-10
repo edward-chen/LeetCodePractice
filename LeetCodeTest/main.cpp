a896274e8bf2e383a7f70b8636b47e23126a326a
edward.chen <jacy22@gmail.com>
2016-03-10 17:03:43 +0800

    Initial Commit

---
+++ LeetCodeTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCodeTest
+//
+//  Created by ChenYi-Hung on 2016/3/10.
+//  Copyright © 2016年 ChenYi-Hung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
