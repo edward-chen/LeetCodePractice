91b425c5248d5992f0185da614d05e83e2bf4fe2
edward.chen <jacy22@gmail.com>
2016-05-05 18:14:36 +0800

    [Title] Test 125 Valid Palindrome
    [Description]
    1. Truncate unnecessary character first, then do the comparison.

---
+++ LeetCodeTest/C125ValidPalindrome.hpp
@@ -0,0 +1,24 @@
+//
+//  C125ValidPalindrome.hpp
+//  LeetCodeTest
+//
+//  Created by ChenYi-Hung on 2016/5/5.
+//  Copyright © 2016年 ChenYi-Hung. All rights reserved.
+//
+
+#ifndef C125ValidPalindrome_hpp
+#define C125ValidPalindrome_hpp
+
+#include <stdio.h>
+#include "BasicDataStructure.h"
+
+using namespace std;
+
+namespace ValidPalindrome {
+    class Solution {
+    public:
+        bool isPalindrome(string s);
+    };
+}
+
+#endif /* C125ValidPalindrome_hpp */
